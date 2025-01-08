e37a9f085ccd2089a097082a2080e1a40c10ca25
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-01-08 13:43:09 +0800

    revise the backround from color to image 👌

--- lib/pages/AboutUS.dart
+++ lib/pages/AboutUS.dart
@@ -10,13 +10,11 @@ class AboutPage extends StatefulWidget {
 }
 
 class aboutState extends State<AboutPage> {
-
-
   int _currentIndex = 3;
 
   @override
   void initState() {
-
+    super.initState();
   }
 
   @override
@@ -28,47 +26,67 @@ class aboutState extends State<AboutPage> {
           'About Us',
           style: TextStyle(color: Colors.white), // Clear, professional text color
         ),
-        backgroundColor: Colors.grey[800], // Light gra
+        backgroundColor: Colors.grey[800], // Light gray background
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const SizedBox(height: 10),
-             Center(
-              child: Container(
-                 height: 200,
-                 width: 200,
-                 decoration: const BoxDecoration(
-                   image: DecorationImage(
-                     image: AssetImage('assets/images/logo2.png'),
-                     fit: BoxFit.fill,
-                   ),
-                 ),
-               ),
-             ),
-            const Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '"Kamayo Translator" . With this application, you may translate from Mandaya to English and vice versa. The software will promote unity among users while also aiding in the preservation of the Mandaya language and culture.'
-                      ' Additionally, the application will support the Mandaya people in preserving their language and cultural identity while promoting cultural awareness among non-Speakers.'
-                      'To guarantee correctness, the application entails gathering and evaluating language data with native speakers. Speech recognition, offline capabilities, and real-time text and voice translation will all be included in the program.'
-                      ' In order to help users better understand Mandaya culture, it will also provide cultural remarks and contextual information. Community input will guarantee that the app satisfies Mandaya\'s requirements.',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontFamily: 'OpenSans',
-
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black,
-                  ),
-                  textAlign: TextAlign.justify,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/map.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Container(
+            width: 320,
+            height: 550,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset(0.0, 1.0),
+                  blurRadius: 6.0,
                 ),
               ],
             ),
-            const SizedBox(height: 20), // Adjusted space here
-          ],
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const SizedBox(height: 10),
+                  Center(
+                    child: Container(
+                      height: 200,
+                      width: 200,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/logo2.png'),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    '"Kamayo Translator" . With this application, you may translate from Mandaya to English and vice versa. The software will promote unity among users while also aiding in the preservation of the Mandaya language and culture.'
+                        ' Additionally, the application will support the Mandaya people in preserving their language and cultural identity while promoting cultural awareness among non-Speakers.'
+                        'To guarantee correctness, the application entails gathering and evaluating language data with native speakers. Speech recognition, offline capabilities, and real-time text and voice translation will all be included in the program.'
+                        ' In order to help users better understand Mandaya culture, it will also provide cultural remarks and contextual information. Community input will guarantee that the app satisfies Mandaya\'s requirements.',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'OpenSans',
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                  const SizedBox(height: 20), // Adjusted space here
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
